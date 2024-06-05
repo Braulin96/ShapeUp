c14e07e983792ed3fa290a02f51f7a121a389c91
Braulin Pires <braulinpires@hotmail.com>
2024-06-05 22:44:43 +0100

    added a radial gradient

--- src/App.js
+++ src/App.js
@@ -1,99 +1,11 @@
-import React, { useState, useEffect } from "react";
-
-import Login from "./components/Login/Login";
-import Spinner from "./utilities/Spinner";
-import useCounter from "./components/hooks/useConter";
-
-import { Users } from "./data/loginMockup";
+import LoginBlock from "./components/Login/LoginBlock";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [userNames, setUsersNames] = useState();
-  const [userEmails, setUsersEmails] = useState();
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentImage, setCurrentImage] = useState();
-  const [error, setError] = useState();
-  const [fetchError, setFetchError] = useState();
-  const [counter, handleCounter] = useCounter ();
-
-  useEffect(() => {
-    const names = Users.map((user) => user.name);
-    setUsersNames(names);
-    const emails = Users.map((user) => user.email);
-    setUsersEmails(emails);
-  }, []);
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const category = "nature";
-
-  const fetchImages = async () => {
-    try {
-      const response = await fetch("https://source.unsplash.com/random");
-      const image = response.url;
-      setCurrentImage(image);
-      setIsLoading(false);
-    } catch (error) {
-      setFetchError(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchImages();
-  }, []);
-
-  const handleError = () => {
-    const nameExists = userNames.includes(name);
-    const emailExists = userEmails.includes(email);
-    if (nameExists && emailExists) {
-      setError(false);
-    } else {
-      setError(true);
-    }
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleError();
-  };
-
-  const isSubmitDisabled = !name || !email;
-
   return (
     <div className="App">
-
-      <button className="bg-blue-500 p-3 rounded-full" onClick={handleCounter}>
-        {counter}
-      </button>
-
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <Login
-            name={name}
-            email={email}
-            handleSubmit={handleSubmit}
-            handleNameChange={handleNameChange}
-            handleEmailChange={handleEmailChange}
-            disabled={isSubmitDisabled}
-          />
-            {error && (
-            <p style={{ color: "red" }}>Error: Invalid name or email</p>
-          )}
-          <div className="rounded-full overflow-hidden w-[300px] mx-auto mt-[20px]">
-            <img src={currentImage} alt="woman" className="bg-cover" />
-          </div>
-        
-        </>
-      )}
+      <>
+        <LoginBlock />
+      </>
     </div>
   );
 };
---
+++ src/components/Login/LoginBlock.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+import "./LoginBlock.css";
+
+const LoginBlock = () => {
+  return <div className="loginBlock w-screen h-screen text-black flex justify-center items-center text-[70px] font-extralight">
+  Login Page
+</div>
+};
+
+LoginBlock.propTypes = {};
+
+export default LoginBlock;
