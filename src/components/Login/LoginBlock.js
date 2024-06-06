a323173f8cebfc4e785db3d9562ee44e55b53fa9
Braulin Pires <braulinpires@hotmail.com>
2024-06-06 22:19:22 +0100

    add a second message after the depresive ones

--- src/components/Login/LoginBlock.js
+++ src/components/Login/LoginBlock.js
@@ -9,6 +9,8 @@ const LoginBlock = () => {
   const landingMockupData = landingMockup();
   const [currentIndex, setCurrentIndex] = useState(0);
   const [showMotivational, setShowMotivational] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   console.log("showMotivational:", showMotivational);
 
   useEffect(() => {
@@ -27,28 +29,42 @@ const LoginBlock = () => {
     }
   }, [currentIndex, landingMockupData.length]);
 
+  const handleAnimationComplete = () => {
+    setIsLoading(true);
+  };
+
   return (
     <motion.div className="loginBlock w-screen h-screen text-white bg-black flex justify-center items-center text-[70px] font-extralight relative">
       <motion.div
-        animate={{ scale: [1,0] }}
-        exit={{ opacity: 0, scale:0 }}
+        animate={{ scale: [1, 0] }}
+        exit={{ opacity: 0, scale: 0 }}
         transition={{
           duration: 2,
           delay: 10,
         }}
-        className="border-2 border-black w-auto h-full aspect-square rounded-full flex justify-center items-center
-       "
+        onAnimationComplete={handleAnimationComplete}
+        className="border-2 border-black w-auto h-full aspect-square rounded-full flex justify-center items-center"
       >
         <motion.div
           key={currentIndex}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.5}}
+          transition={{ duration: 0.5 }}
         >
           {landingMockupData[currentIndex]}
         </motion.div>
       </motion.div>
+      {isLoading && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ scale: [0, 1], opacity: [0, 1] }}
+          transition={{ duration: 1 }}
+          className="absolute"
+        >
+          Loading your journey
+        </motion.div>
+      )}
     </motion.div>
   );
 };
